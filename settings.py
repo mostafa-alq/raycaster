4c18888da043356ad252bcd361c7279e9d1cc787
Mostafa <109628361+mostafa-alq@users.noreply.github.com>
2024-04-09 21:57:05 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 # main game settings :)
 # capitalised = const
 
+import math
+
 RES = WIDTH, HEIGHT = 1600, 900
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
 FPS = 100
 
 # initialise player stuff
@@ -10,3 +14,13 @@ PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
 PLAYER_ROT_SPEED = 0.002
 PLAYER_SIZE_SCALE = 60
+
+FOV = 1.2 * math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
+
+SCREEN_DIST = HALF_WIDTH / math.tan(HALF_FOV)
+SCALE = WIDTH // NUM_RAYS
